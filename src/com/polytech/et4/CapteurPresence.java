d48f045c0db96482e167a8ebee4024890d807dfa
Gaulois94 <serenomickael@gmail.com>
2016-12-09 10:33:04 +0000

    Add presence capteur

--- src/com/polytech/et4/CapteurPresence.java
+++ src/com/polytech/et4/CapteurPresence.java
@@ -5,17 +5,9 @@ import com.polytech.et4.ElemRegulation;
 public class CapteurPresence extends Capteur {
 
 	private boolean m_est_present = false;
-	private boolean m_estNotifie  = false;
 	
 	public boolean  getPresence()
 	{
-		if(m_estNotifie)
-		{
-			return m_est_present = true;
-		}
-		else
-		{
-			return m_est_present = false;
-		}		
+		return m_est_present;
 	}
 }
