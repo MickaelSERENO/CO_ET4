83e12002a2a9bd52d4720cabf2d5224e27eddcfd
Laure <cyrille.laure@gmail.com>
2016-12-10 08:48:28 +0100

    Update Capteur

--- src/com/polytech/et4/Capteur.java
+++ src/com/polytech/et4/Capteur.java
@@ -1,9 +1,18 @@
-package com.polytech.et4;
+package tp_Muscle;
+import tp_Muscle.ElemRegulationCapteur;
 
-import com.polytech.et4.ElemRegulationCapteur;
-
-public class Capteur {
+public class Capteur extends Element implements Updatable {
     ElemRegulationCapteur m_elem=null;
+	int c_position =0;
+    private SegmentRoute m_segmentRoute = null;
+    private int m_sens=0;
+    
+    public Capteur(int position, int sens)
+    {
+    	super(position, sens);
+		c_position = position;
+		m_sens=sens;
+    }
     
     public ElemRegulationCapteur getElemRegulation() 
     {
@@ -13,5 +22,22 @@ public class Capteur {
     public void setElemRegulation(ElemRegulationCapteur m_elem) 
     {
         this.m_elem = m_elem;
-    }    
+    }
+
+	@Override
+	public void prochaineEtape() {
+		// Maj de la position du capteur vers le prochain capteur
+		c_position = c_position + ((m_sens == ARRIERE) ? -1 : 1);
+		
+		SegmentRoute prochaineRoute = m_segmentRoute.setPosition(this);
+		
+		if(prochaineRoute != m_segmentRoute)
+		{
+			m_position = (m_sens == ARRIERE) ? prochaineRoute.getPositionFin() : prochaineRoute.getPositionDebut();
+			m_segmentRoute = prochaineRoute
+		}
+		
+	}    
+
+
 }
